bf4699a6f7f2af47d6507217cdff9bc88a48eee9
Chase Moskal <chasemoskal@gmail.com>
2016-02-11 21:49:17 -0800

    Tooling up.

---
+++ index.ts
@@ -0,0 +1,3 @@
+import Check from './modules/Check';
+console.log("Check:", new Check);
+window["Check"] = Check;
---
+++ modules/Check.ts
@@ -0,0 +1,4 @@
+export default class Check {
+    static check: boolean = true;
+    check() { return true; }
+}
