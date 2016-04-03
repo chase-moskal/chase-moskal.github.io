c892817874e1b6ac501cb92dc1403a95e936a6fa
Chase Moskal <chasemoskal@gmail.com>
2016-04-03 13:30:19 -0700

    Fix email address antispam.

--- source/modules/antiSpam.ts
+++ source/modules/antiSpam.ts
@@ -3,6 +3,10 @@
  * Run through the DOM and inject Base64-encoded email addresses.
  */
 export default function antiSpam(document: Document = window.document) {
-    for (let a of [].slice.call(document.querySelectorAll("[data-email]")))
-        a.href = a.textContent = atob(a.getAttribute("data-email"));
+    const emailLinks: HTMLAnchorElement[] = [].slice.call( document.querySelectorAll("a[data-email]") );
+    for (const a of emailLinks) {
+        const email = atob(a.getAttribute("data-email"));
+        a.href = `mailto:${email}`;
+        a.textContent = email;
+    }
 }
